e3b56aa71341d24fdc82f0a3cc20eeed5c29326a
DinaraMurz <dinara_myrzabek@mail.ru>
2020-03-09 05:43:22 +0600

    first commit

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+
+include_once "Models/Tag.php";
+$tag = new Tag('a');
+
+
+$tag->prependBody('нажми :D');
+$tag->addAttribute('href', 'http://google.com');
+?>
+
+
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tag Lesson</title>
+</head>
+<body>
+    <?= $tag->getString() ?>
+</body>
+</html>
